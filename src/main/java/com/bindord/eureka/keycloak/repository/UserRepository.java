7a84dfa6afe84ace7aaafb5ed153599b5c4f0e62
Petter Carranza <peterpaul.0194@gmail.com>
2024-01-13 19:43:42 -0500

    raw version of migration of latest keycloak version finished

--- src/main/java/com/bindord/eureka/keycloak/repository/UserRepository.java
+++ src/main/java/com/bindord/eureka/keycloak/repository/UserRepository.java
@@ -5,10 +5,15 @@ import com.bindord.eureka.keycloak.domain.dto.UserPasswordDTO;
 import com.bindord.eureka.keycloak.domain.request.EurekaUser;
 import com.bindord.eureka.keycloak.domain.request.UserLogin;
 import com.bindord.eureka.keycloak.domain.response.UserToken;
+import com.bindord.eureka.keycloak.generic.KeycloakClientIds;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.ClientResource;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.RoleResource;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.authorization.client.AuthzClient;
 import org.keycloak.authorization.client.Configuration;
@@ -16,6 +21,7 @@ import org.keycloak.authorization.client.util.Http;
 import org.keycloak.representations.AccessTokenResponse;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.ErrorRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,7 +30,9 @@ import org.springframework.stereotype.Repository;
 
 import javax.ws.rs.core.Response;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.bindord.eureka.keycloak.util.Constants.OAUTH2_CLIENT_ID;
 import static com.bindord.eureka.keycloak.util.Constants.OAUTH2_CLIENT_SECRET;
@@ -42,9 +50,12 @@ public class UserRepository {
 
     private final Configuration keycloakConfig;
 
-    public UserRepository(Keycloak keycloak, Configuration keycloakConfig) {
+    private final KeycloakClientIds keycloakClientIds;
+
+    public UserRepository(Keycloak keycloak, Configuration keycloakConfig, KeycloakClientIds keycloakClientIds) {
         this.keycloak = keycloak;
         this.keycloakConfig = keycloakConfig;
+        this.keycloakClientIds = keycloakClientIds;
     }
 
     @Value("${keycloak.realm}")
@@ -74,6 +85,32 @@ public class UserRepository {
 
         userRepresentation.setCredentials(List.of(credential));
         userRepresentation.setEnabled(true);
+        // Get the client resource
+        var clientOauth = realmResource.clients().get(keycloakClientIds.getClientId());
+        System.out.println(clientOauth.roles());
+        realmResource.clients().findAll().forEach(e-> {
+            if(keycloakClientIds.getClientId().equals(e.getId())) {
+                try {
+                    System.out.println(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(e));
+                } catch (JsonProcessingException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            System.out.println(e.getId());
+            System.out.println(e.getName());
+        });
+        ClientResource clientResource = clientOauth;
+// Get the role resource for each role
+        clientResource.roles().list().forEach(System.out::println);
+        RoleResource customerRoleResource = clientResource.roles().get("service_full");
+//        RoleResource adminRoleResource = clientResource.roles().get("adminx_jaipro_role");
+        // Get the role representations
+        RoleRepresentation customerRoleRepresentation = customerRoleResource.toRepresentation();
+//        RoleRepresentation adminRoleRepresentation = adminRoleResource.toRepresentation();
+
+        Map<String, List<String>> attributes = new HashMap<>();
+        attributes.put("profile", List.of("specialist"));
+        userRepresentation.setAttributes(attributes);
 
         Response response = realmResource.users().create(userRepresentation);
         var resCode = response.getStatus();
@@ -81,6 +118,13 @@ public class UserRepository {
             var location = response.getLocation().toString();
             var userId = Lists.reverse(Arrays.asList(location.split("/"))).get(0);
             userRepresentation.setId(userId);
+
+
+            // Get the user resource
+            UserResource userResource = realmResource.users().get(userId);
+            userResource.roles().clientLevel(keycloakClientIds.getClientId()).add(List.of(customerRoleRepresentation/*, adminRoleRepresentation*/));
+
+
             return userRepresentation;
         }
         var errDetail = response.readEntity(ErrorRepresentation.class);
